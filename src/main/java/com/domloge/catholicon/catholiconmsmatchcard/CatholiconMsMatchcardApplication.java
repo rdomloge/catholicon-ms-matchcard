ce9592ee6ee2bf95f1dfff5e6bc92042619b61da
Ramsay Domloge <ramsay.domloge@bca.com>
2019-02-07 21:47:32 +0000

    Initial commit

---
+++ src/main/java/com/domloge/catholicon/catholiconmsmatchcard/CatholiconMsMatchcardApplication.java
@@ -0,0 +1,14 @@
+package com.domloge.catholicon.catholiconmsmatchcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatholiconMsMatchcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatholiconMsMatchcardApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/domloge/catholicon/catholiconmsmatchcard/CatholiconMsMatchcardApplicationTests.java
@@ -0,0 +1,17 @@
+package com.domloge.catholicon.catholiconmsmatchcard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatholiconMsMatchcardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
