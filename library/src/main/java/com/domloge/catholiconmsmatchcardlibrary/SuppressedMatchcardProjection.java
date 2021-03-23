eb124c75966e08cde1c0007823ba47cc38346f79
Ramsay Domloge <rdomloge@gmail.com>
2021-03-23 00:20:04 +0000

    Simplified projections and binned some unused classes

--- library/src/main/java/com/domloge/catholiconmsmatchcardlibrary/SuppressedMatchcardProjection.java
+++
@@ -1,8 +0,0 @@
-package com.domloge.catholicon.catholiconmsmatchcard;
-
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "suppressedMatchcardProjection", types = { Matchcard.class })
-public interface SuppressedMatchcardProjection {
-
-}
